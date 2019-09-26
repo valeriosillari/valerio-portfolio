7a0fd445ac5f9524772f8c8b60cc551b827e96df
Valerio Sillari <valerio.sillari@gmail.com>
2019-09-26 15:11:04 +0200

    set ine test for screenshots

--- src/views/layouts/layout.test.js
+++ src/views/layouts/layout.test.js
@@ -1,12 +1,12 @@
 const { toMatchImageSnapshot } = require('jest-image-snapshot')
 
-function sum(a, b) {
-  return a + b
-}
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
+// function sum(a, b) {
+//   return a + b
+// }
+//
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3)
+// })
 
 test('screenshots', () => {
   expect.extend({ toMatchImageSnapshot })
