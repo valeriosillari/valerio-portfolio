cfb04e1939f6c1f20d416f3c1301d8ec0a677105
Valerio Sillari <valerio.sillari@gmail.com>
2019-09-26 15:10:25 +0200

    set all tests for jest into layout file

--- jest-setup.js
+++ jest-setup.js
@@ -14,6 +14,3 @@
 
 // eslint runs from root and only looks at root package.json
 // eslint-disable-next-line import/no-unresolved
-const { toMatchImageSnapshot } = require('jest-image-snapshot')
-
-expect.extend({ toMatchImageSnapshot })
