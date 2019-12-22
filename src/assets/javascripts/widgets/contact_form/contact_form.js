412053ff49a3aa1b1e17ac064c654c0a0ea1ce50
Valerio Sillari <valerio.sillari@gmail.com>
2019-12-22 23:09:05 +0100

    test mailer with dev url

--- src/assets/javascripts/widgets/contact_form/contact_form.js
+++ src/assets/javascripts/widgets/contact_form/contact_form.js
@@ -1,7 +1,10 @@
 import $ from 'jquery'
 
 const urlMailServerValidation =
-  'https://express-contact-form.herokuapp.com/submit'
+  // mailer prod
+  // 'https://express-contact-form.herokuapp.com/submit'
+  // mailer dev
+  'https://express-contact-form-develop.herokuapp.com/submit'
 
 const btnSubmitElement = '.js-form-btn-submit'
 const buttonSubmitSendingClass = 'js-form-submit-sending'
