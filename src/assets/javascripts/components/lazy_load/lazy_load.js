170489485824de4e7e53768672c885dd642af7f7
Valerio Sillari <valerio.sillari@gmail.com>
2020-11-08 22:57:48 +0100

    update lazy sizes

--- src/assets/javascripts/components/lazy_load/lazy_load.js
+++ src/assets/javascripts/components/lazy_load/lazy_load.js
@@ -1,7 +1,5 @@
-import 'node_lazy_sizes'
+import lazySizes from 'node_lazy_sizes'
 
-// set to only load visible elements by minimizing the preload viewport
-window.lazySizesConfig = window.lazySizesConfig || {}
-window.lazySizesConfig.lazyClass = 'js-lazy'
-window.lazySizesConfig.loadedClass = 'js-lazy-loaded'
-window.lazySizesConfig.expand = -20
+lazySizes.cfg.lazyClass = 'js-lazy'
+lazySizes.cfg.loadedClass = 'js-lazy-loaded'
+lazySizes.cfg.expand = -20
