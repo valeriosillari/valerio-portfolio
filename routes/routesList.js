30f063851daae61d44af481b0644cc90d2b0c7dc
Valerio Sillari <valerio.sillari@gmail.com>
2020-12-22 23:47:11 +0100

    update code: now info about now/vercel

--- routes/routesList.js
+++ routes/routesList.js
@@ -2,8 +2,6 @@ import express from 'express'
 
 const router = express.Router()
 
-// if push DIST folder all good on NOW ZEIT
-
 // GET home page.
 router.get('/', (req, res, next) => {
   res.render('index')
