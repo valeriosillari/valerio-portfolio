6523d568ca3d5f5751810703b8a5271dfa4107c0
Valerio Sillari <valerio.sillari@gmail.com>
2023-08-07 01:22:26 +0200

    delete all files, except git

--- config/gulp/task_assets.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp'
-import staticFiles from './task_static_files'
-import styles from './task_styles'
-import modernizr from './task_modernizr'
-import scripts from './task_scripts'
-
-const assets = gulp.series(staticFiles, styles, modernizr, scripts)
-
-export default assets
--- src/assets/javascripts/widgets/navigation/navigation_headroom.js
+++
@@ -1,9 +0,0 @@
-import Headroom from 'node_headroom'
-
-// grab an element
-const headroomElement = document.querySelector('.js-headroom')
-
-// initialise
-if (headroomElement) {
-  new Headroom(headroomElement).init()
-}
