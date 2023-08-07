9ae1b9f834fad8fa3055ecb278cbdfd77117e10e
Valerio Sillari <valerio.sillari@gmail.com>
2023-08-08 00:25:54 +0200

    css bem naming and indent

--- components/molecules/splashscreen.vue
+++ components/molecules/splashscreen.vue
@@ -4,7 +4,7 @@
 -->
 
 <template>
-    <div class="splash-screen" v-if="isSplashscreenActive">
+    <div class="b-splash-screen" v-if="isSplashscreenActive">
         <div class="spinner-wrapper">
             <div class="spinner"></div>
         </div>
@@ -20,39 +20,27 @@ onMounted(() => {
 </script>
 
 <style lang="scss">
-.splash-screen {
+.b-splash-screen {
     background: #f2f0ee;
     width: 100vw;
     height: 100vh;
     position: fixed;
     z-index: 50;
-}
 
-.spinner-wrapper {
-    position: absolute;
-    left: 50%;
-    top: 50%;
+    .spinner-wrapper {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
 
-    transform: translate(-50%, -50%);
-}
-.spinner {
-    width: 80px;
-    height: 80px;
-    margin: 100px auto;
-    background-color: #e45447;
-
-    border-radius: 100%;
-    -webkit-animation: sk-scaleout 1s infinite ease-in-out;
-    animation: sk-scaleout 1s infinite ease-in-out;
-}
-
-@-webkit-keyframes sk-scaleout {
-    0% {
-        -webkit-transform: scale(0);
-    }
-    100% {
-        -webkit-transform: scale(1);
-        opacity: 0;
+        .spinner {
+            width: 80px;
+            height: 80px;
+            margin: 100px auto;
+            background-color: #e45447;
+            border-radius: 100%;
+            animation: sk-scaleout 1s infinite ease-in-out;
+        }
     }
 }
 
