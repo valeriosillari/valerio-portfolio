4392a1ae8f55b8c170e31d7c54f91fc5a65c64af
Valerio Sillari <valerio.sillari@gmail.com>
2023-08-12 00:24:48 +0200

    i got something

--- components/molecules/heading-type-writer.vue
+++ components/molecules/heading-type-writer.vue
@@ -1,5 +1,51 @@
 <template>
-    <p class="b-heading-type-writer">
-        Development Consultant. Frontend Developer. One day Jedi.
-    </p>
+    <p class="b-heading-type-writer" ref="typing"></p>
 </template>
+
+<!-- Vue/Nuxt 3 Implementation -->
+<!-- https://github.com/Orlandster/vue-typed-js/issues/41 -->
+<script setup>
+import Typed from 'typed.js'
+
+const typing = ref(null)
+
+const textList = [
+    `<span class="type-writer-text-01">
+        Development Consultant
+    </span>`,
+    `<span class="type-writer-text-02">
+        Frontend Coder
+    </span>`,
+    `<span class="type-writer-text-03">
+        One day Jedi'
+    </span>`,
+]
+
+onMounted(() => {
+    new Typed(typing.value, {
+        strings: textList,
+        typeSpeed: 100,
+        // backSpeed: 0,
+        loop: true,
+        showCursor: false,
+        fadeOut: true,
+    })
+})
+</script>
+
+<style lang="scss">
+.b-heading-type-writer {
+    // 2 lines of text, magic number
+    min-height: 110px;
+
+    .type-writer-text-01 {
+        color: orange;
+    }
+    .type-writer-text-02 {
+        color: red;
+    }
+    .type-writer-text-03 {
+        color: magenta;
+    }
+}
+</style>
