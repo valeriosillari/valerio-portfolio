41ea8f69b7c65fd31940a38dcf2927b61c45a778
Valerio Sillari <valerio.sillari@gmail.com>
2023-08-12 01:30:50 +0200

    text fix

--- components/organisms/sections/section-start.vue
+++ components/organisms/sections/section-start.vue
@@ -2,7 +2,7 @@
     <OrganismsSectionsSectionPrototype classnames="is-section-start">
         <MoleculesAvatarSvgShape />
 
-        <h1>Hello I'm Valerio.</h1>
+        <h1>Hello, I'm Valerio.</h1>
 
         <MoleculesHeadingTypeWriter />
     </OrganismsSectionsSectionPrototype>
