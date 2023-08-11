62c031e437d3168db75e3e34b544fe08f9e582f6
Valerio Sillari <valerio.sillari@gmail.com>
2023-08-11 14:41:47 +0200

    set basic link component

---
+++ components/atoms/link-external.vue
@@ -0,0 +1,40 @@
+<template>
+    <a
+        :class="['b-link-external', props.classnames]"
+        :href="props.url"
+        :title="props.title"
+        :target="isTargetBlank ? '_blank' : null"
+    >
+        {{ props.text }}
+    </a>
+</template>
+
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+
+    url: {
+        type: String,
+        required: true,
+    },
+
+    title: {
+        type: String,
+        // another prop as default value
+        default: (props) => props.text,
+    },
+
+    isTargetBlank: {
+        type: Boolean,
+        default: true,
+    },
+
+    classnames: {
+        type: String,
+        default: null,
+    },
+})
+</script>
