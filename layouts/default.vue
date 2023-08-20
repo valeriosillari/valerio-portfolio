848a17b1aabb04b6aaf238a0b42fbfa7f4e27817
Valerio Sillari <valerio.sillari@gmail.com>
2023-08-21 01:25:34 +0200

    layout now handle page transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,3 +9,16 @@
         <OrganismsFooter />
     </div>
 </template>
+
+<style lang="scss">
+.page-enter-active,
+.page-leave-active {
+    transition: all 0.5s;
+}
+
+.page-enter-from,
+.page-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+}
+</style>
