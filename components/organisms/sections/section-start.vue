3cba67f208dc9da0c24e463840bcab9962c49455
Valerio Sillari <valerio.sillari@gmail.com>
2024-12-08 00:40:01 +0100

    got a very basic bg with video (star)

--- components/organisms/sections/section-start.vue
+++ components/organisms/sections/section-start.vue
@@ -1,17 +1,77 @@
 <template>
-    <div :class="['b-section', 'is-section-start YOOOLO']">
-        <div class="container" v-bind="animationAttributes">
-            <MoleculesAvatarSvgShape data-aos="fade-up" data-aos-delay="500" />
+    <div :class="['b-section', 'is-section-start is-with-video-bg YOOOLO']">
+        <video
+            autoplay
+            muted
+            class="background-video"
+            ref="backgroundVideo"
+            @timeupdate="handleTimeUpdate"
+        >
+            <source
+                src="/videos/1926295-hd_1920_1080_25fps.mp4"
+                type="video/mp4"
+            />
+            Your browser does not support the video tag.
+        </video>
 
-            <div class="heading" data-aos="fade-up" data-aos-delay="1800">
-                <h1>Hello, I'm Valerio.</h1>
+        <div class="content">
+            <div class="container" v-bind="animationAttributes">
+                <MoleculesAvatarSvgShape
+                    data-aos="fade-up"
+                    data-aos-delay="500"
+                />
 
-                <MoleculesHeadingTypeWriter />
+                <div class="heading" data-aos="fade-up" data-aos-delay="1800">
+                    <h1>Hello, I'm Valerio.</h1>
+
+                    <MoleculesHeadingTypeWriter />
+                </div>
             </div>
         </div>
     </div>
 </template>
 
+<style lang="scss">
+.b-section {
+    &.is-with-video-bg {
+        padding: 0;
+        background-color: red;
+        position: relative;
+        width: 100%; /* Full width of the parent */
+        height: 100vh; /* Full viewport height or customize as needed */
+        overflow: hidden; /* Ensure the video does not overflow the container */
+
+        .background-video {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            min-width: 100%;
+            min-height: 100%;
+            width: auto;
+            height: auto;
+            // z-index: -1;
+            opacity: 0.15;
+            object-fit: cover; /* Ensures the video always covers the container */
+        }
+
+        .content {
+            position: relative;
+        }
+    }
+}
+</style>
+
 <script setup>
-// const aosElementBasicTiming = ref(250)
+// Ref for the video element
+const backgroundVideo = ref(null)
+
+let isReversing = false
+
+onMounted(() => {
+    if (backgroundVideo.value) {
+        // Set playback rate to slow down the video
+        backgroundVideo.value.playbackRate = 0.3 // Adjust this value as needed
+    }
+})
 </script>
